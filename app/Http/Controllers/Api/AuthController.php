70cf201da761c8f1057dab7e6f41c078e6e77570
RidwanRizqi <Ridwanrizqi64@gmail.com>
2023-06-13 10:55:34 +0700

    Done Test Logout

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -11,7 +11,7 @@ use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Response;
+use Illuminate\Http\Response;
 
 class AuthController extends Controller
 {
@@ -50,5 +50,18 @@ class AuthController extends Controller
             'token_type' => 'Bearer',
             'user' => $user
         ]);
+    }
+
+    public function logout(Request $request)
+    {
+        try {
+            auth()->user()->tokens()->delete();
+            return $this->apiSuccess('Tokens Revoked');
+        } catch (\Throwable $e) {
+            throw new HttpResponseException($this->apiError(
+                null,
+                Response::HTTP_INTERNAL_SERVER_ERROR,
+            ));
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,7 @@ Route::post('/login', [AuthController::class, 'login']);
 //    return $request->user();
 //});
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
